3cb152a87f3574e4239e53e39f79c0c872adc055
Slim Abida <slimabida21@gmail.com>
2026-01-09 17:55:48 +0100

    creartion de la page NotFound test Ok

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="page-container">
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to="/" className="btn">Go back to the home page</Link>
+    </div>
   )
 }
